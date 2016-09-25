29587a58d6ad782695162bb43de4ace15be84e0b
Yuzhou Liu <lyz_269@hotmail.com>
2016-09-25 12:30:13 -0400

    Renames files by keyword in batches in Windows
    Script to rename windows files in bulk. Useful for getting rid of
    annoying words in filenames from torrents.

---
+++ file_rename.py
@@ -0,0 +1,39 @@
+import os
+
+# Done string
+done_str = "done"
+
+# Get the directory to search
+file_dir = raw_input("Enter the directory path (c/p path): ")
+# Get file extension to look for
+file_ext = raw_input("File Extension: ")
+# List to store keywords to look for
+search_list = []
+
+# Get the list of keywords to search
+while True:
+
+    search_str = raw_input("Keyword to search (enter 'done' when finished): ")
+    if search_str == done_str:
+        break;
+
+    search_list.append(search_str)
+
+# Go through each file that ends with the matching file extension
+for filename in os.listdir(file_dir):
+    if filename.endswith(file_ext):
+
+        # Capture the filename
+        new_filename = filename
+
+        # Iterate through the list and search for keyword
+        for keyword in search_list:
+            # Replace strings as necessary
+            new_filename = new_filename.replace(keyword, '')
+        
+        # Strip left and right whitespace
+        new_filename = new_filename.lstrip()
+        new_filename = new_filename.rstrip()
+
+        # Finally rename the file
+        os.rename( os.path.join(file_dir, filename), os.path.join(file_dir, new_filename) )
